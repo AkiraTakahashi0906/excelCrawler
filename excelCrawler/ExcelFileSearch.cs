b379790387260dc8190ba6c6a455631bbdbcc9dd
akira takahashi <akira.takahashi.s2000@gmail.com>
2020-10-01 20:33:25 +0900

    プロジェクト ファイルを追加します。

---
+++ excelCrawler/ExcelFileSearch.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excelCrawler
+{
+    public static class ExcelFileSearch
+    {
+        public static List<string> GetFileName(string path)
+        {
+            var list = new List<string>();
+            var targetDirectorys = GetFolderPath(path);
+            foreach (var directorys in targetDirectorys)
+            {
+                string[] fileNames = Directory.GetFiles(directorys.FullName);
+                foreach (var name in fileNames)
+                {
+                    if (IsExcel(name))
+                    {
+                        list.Add(name);
+                        Console.WriteLine(name);
+                    }
+                }
+            }
+            return list;
+        }
+
+        private static DirectoryInfo[] GetFolderPath(string path)
+        {
+            DirectoryInfo di = new DirectoryInfo(path);
+            return di.GetDirectories("*", SearchOption.AllDirectories);
+        }
+
+        private static bool IsExcel(string fullpath)
+        {
+            var ext = Path.GetExtension(fullpath);
+            if (ext == ".cs" || ext == ".cs")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ excelCrawler/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excelCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var targetPath = @"C:\Users\akira\OneDrive\デスクトップ\C#\asynchronous_study";
+            try
+            {
+                ExcelFileSearch.GetFileName(targetPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
